a18927fd68b8467ee4f9876ee32e239706dff9c2
Qiankai Lin <linqiankai@geweian.com>
2025-03-22 13:03:12 +0800

    add sentinel client for redis enrichment_table
    fix bug
    
    fix redis bug

--- src/sources/redis/channel.rs
+++ src/sources/redis/channel.rs
@@ -22,25 +22,15 @@ impl InputHandler {
         mut self,
         connection_info: ConnectionInfo,
     ) -> crate::Result<Source> {
-        let conn = self
-            .client
-            .get_async_connection()
-            .await
-            .context(ConnectionSnafu {})?;
-
-        trace!(endpoint = %connection_info.endpoint.as_str(), "Connected.");
-
-        let mut pubsub_conn = conn.into_pubsub();
-        pubsub_conn
+        let (mut sink, mut stream) = self.client.get_async_pubsub().await.context(ConnectionSnafu {})?.split();
+        sink
             .subscribe(&self.key)
             .await
             .context(SubscribeSnafu {})?;
         trace!(endpoint = %connection_info.endpoint.as_str(), channel = %self.key, "Subscribed to channel.");
 
         Ok(Box::pin(async move {
-            let shutdown = self.cx.shutdown.clone();
-            let mut pubsub_stream = pubsub_conn.on_message().take_until(shutdown);
-            while let Some(msg) = pubsub_stream.next().await {
+            while let Some(msg) = stream.next().await {
                 match msg.get_payload::<String>() {
                     Ok(line) => {
                         if let Err(()) = self.handle_line(line).await {
