c31b219507286fd19065e048b6f9aa1519dca0ae
Qiankai Lin <linqiankai@geweian.com>
2025-03-22 13:03:12 +0800

    add sentinel client for redis enrichment_table
    fix bug

--- src/enrichment_tables/redis.rs
+++ src/enrichment_tables/redis.rs
@@ -1,7 +1,6 @@
 //! Handles enrichment tables for `type = redis`.
 
 use crate::config::{EnrichmentTableConfig, GenerateConfig};
-use futures::StreamExt;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
@@ -15,12 +14,16 @@ async fn subscribe(
     keys: Vec<String>,
     db: u8,
     cache: Arc<RwLock<HashMap<String, ObjectMap>>>,
-    client: redis::Client,
+    mut conn: redis::aio::MultiplexedConnection,
+    mut pubsub: tokio::sync::mpsc::UnboundedReceiver<redis::PushInfo>,
 ) -> Result<(), backoff::Error<redis::RedisError>> {
-    let mut conn = client.get_async_connection().await.map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
+    info!("Starting Redis enrichment table for keys: {:?}", keys);
     for key in &keys {
-        let datas: Option<HashMap<String, String>> =
-            redis::cmd("HGETALL").arg(key).query_async(&mut conn).await.map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
+        let datas: Option<HashMap<String, String>> = redis::cmd("HGETALL")
+            .arg(key)
+            .query_async(&mut conn)
+            .await
+            .map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
         if let Some(datas) = datas {
             for (k, v) in datas {
                 let mut obj = ObjectMap::new();
@@ -30,37 +33,73 @@ async fn subscribe(
             }
         }
     }
-    let mut pubsub = client
-        .get_async_connection()
-        .await
-        .map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?
-        .into_pubsub();
     for key in &keys {
-        let _ = pubsub
+        let _ = conn
             .psubscribe(format!("__keyspace@{}__:{}", db, key))
             .await
             .map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
     }
-    let mut stream = pubsub.on_message();
-    while let Some(msg) = stream.next().await {
-        let channel: String = msg.get_channel_name().to_string();
-        if let Some(key) = channel.strip_prefix(&format!("__keyspace@{}__:", db)) {
-            let datas: Option<HashMap<String, String>> = redis::cmd("HGETALL")
-                .arg(key)
-                .query_async(&mut conn)
-                .await
-                .map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
-            if let Some(datas) = datas {
-                for (k, v) in datas {
-                    let mut obj = ObjectMap::new();
-                    obj.insert("key".into(), Value::from(key.to_string()));
-                    obj.insert("value".into(), Value::from(v));
-                    cache.write().unwrap().insert(k.clone(), obj);
-                }
+    loop {
+        let msg = pubsub.recv().await;
+        if let Some(msg) = msg {
+            if msg.kind == redis::PushKind::Disconnection {
+                return Err(backoff::Error::retry_after(
+                    redis::RedisError::from(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Redis connection closed",
+                    )),
+                    RETRY_AFTER,
+                ));
             }
+            if !msg.data.is_empty() && msg.kind == redis::PushKind::PMessage {
+                let key: Option<String> = msg
+                    .data
+                    .into_iter()
+                    .filter_map(|v| {
+                        if let redis::Value::BulkString(s) = v {
+                            let s = String::from_utf8(s).unwrap();
+                            if s.starts_with(&format!("__keyspace@{}__:", db)) {
+                                Some(
+                                    s.strip_prefix(&format!("__keyspace@{}__:", db))
+                                        .unwrap()
+                                        .to_string(),
+                                )
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<String>>()
+                    .first()
+                    .cloned();
+                if let Some(key) = key {
+                    let datas: Option<HashMap<String, String>> = redis::cmd("HGETALL")
+                        .arg(&key)
+                        .query_async(&mut conn)
+                        .await
+                        .map_err(|e| backoff::Error::retry_after(e, RETRY_AFTER))?;
+                    if let Some(datas) = datas {
+                        for (k, v) in datas {
+                            let mut obj = ObjectMap::new();
+                            obj.insert("key".into(), Value::from(key.clone()));
+                            obj.insert("value".into(), Value::from(v));
+                            cache.write().unwrap().insert(k.clone(), obj);
+                        }
+                    }
+                }
+            };
+        } else {
+            return Err(backoff::Error::retry_after(
+                redis::RedisError::from(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Redis connection closed",
+                )),
+                RETRY_AFTER,
+            ));
         }
     }
-    Ok(())
 }
 
 /// Configuration for the `redis` enrichment table.
@@ -69,24 +108,24 @@ async fn subscribe(
 pub struct RedisConfig {
     /// The host of the Redis server.
     pub host: String,
-    /// The port of the Redis server.
-    pub port: u16,
     /// The password of the Redis server.
     pub password: Option<String>,
     /// The database of the Redis server.
     pub db: u8,
     /// The keys of the Redis server.
     pub keys: Vec<String>,
+    /// The sentinel master name.
+    pub sentinel_master: Option<String>,
 }
 
 impl GenerateConfig for RedisConfig {
     fn generate_config() -> toml::Value {
         toml::Value::try_from(Self {
-            host: "localhost".to_string(),
-            port: 6379,
+            host: "localhost:6379".to_string(),
             password: None,
             db: 0,
             keys: vec![],
+            sentinel_master: None,
         })
         .unwrap()
     }
@@ -98,6 +137,24 @@ impl EnrichmentTableConfig for RedisConfig {
         _: &crate::config::GlobalOptions,
     ) -> crate::Result<Box<dyn Table + Send + Sync>> {
         Ok(Box::new(Redis::new(self.clone())?))
+    }
+}
+
+fn get_redis_url(host: String, password: Option<String>, db: Option<u8>) -> String {
+    if password.is_some() {
+        let mut url = format!("redis://{}@{}", password.unwrap(), host);
+        if let Some(db) = db {
+            url = format!("{}/{}", url, db);
+        }
+        url = format!("{}/?protocol=resp3", url);
+        url
+    } else {
+        let mut url = format!("redis://{}", host);
+        if let Some(db) = db {
+            url = format!("{}/{}", url, db);
+        }
+        url = format!("{}/?protocol=resp3", url);
+        url
     }
 }
 
@@ -119,26 +176,77 @@ impl Redis {
             return Err("Redis keys cannot be empty".into());
         }
 
-        let url = if let Some(password) = &config.password {
-            format!(
-                "redis://:{}@{}:{}/{}",
-                password, config.host, config.port, config.db
-            )
+        let mut client: Option<redis::Client> = None;
+        let mut sentinel: Option<redis::sentinel::SentinelClient> = None;
+        if let Some(sentinel_master) = &config.sentinel_master {
+            let urls = config
+                .host
+                .split(",")
+                .map(|s| get_redis_url(s.to_string(), config.password.clone(), None))
+                .collect::<Vec<String>>();
+            sentinel = Some(
+                redis::sentinel::SentinelClient::build(
+                    urls,
+                    sentinel_master.clone(),
+                    Some(redis::sentinel::SentinelNodeConnectionInfo {
+                        tls_mode: Some(redis::TlsMode::Insecure),
+                        redis_connection_info: Some(redis::RedisConnectionInfo {
+                            db: config.db as i64,
+                            password: config.password.clone(),
+                            ..Default::default()
+                        }),
+                    }),
+                    redis::sentinel::SentinelServerType::Master,
+                )
+                .unwrap(),
+            );
         } else {
-            format!("redis://{}:{}/{}", config.host, config.port, config.db)
-        };
-        let client = redis::Client::open(url)?;
+            let url = get_redis_url(
+                config.host.clone(),
+                config.password.clone(),
+                Some(config.db),
+            );
+            client = Some(redis::Client::open(url)?);
+        }
         let cache = Arc::new(RwLock::new(HashMap::new()));
         let cache_clone = cache.clone();
         let config_clone = config.clone();
 
         tokio::spawn(async move {
             loop {
-                let res = subscribe(config_clone.keys.clone(), config_clone.db, cache_clone.clone(), client.clone()).await;
+                let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+                let config = redis::AsyncConnectionConfig::new().set_push_sender(tx);
+                let res = if let Some(sentinel) = &mut sentinel {
+                    sentinel.get_async_connection_with_config(&config).await
+                } else {
+                    client
+                        .as_mut()
+                        .unwrap()
+                        .get_multiplexed_async_connection_with_config(&config)
+                        .await
+                };
+                let conn = match res {
+                    Ok(conn) => conn,
+                    Err(e) => {
+                        error!("Failed to get Redis connection: {}", e);
+                        tokio::time::sleep(RETRY_AFTER).await;
+                        continue;
+                    }
+                };
+                let res: Result<(), backoff::Error<redis::RedisError>> = subscribe(
+                    config_clone.keys.clone(),
+                    config_clone.db,
+                    cache_clone.clone(),
+                    conn,
+                    rx,
+                )
+                .await;
                 if let Err(e) = res {
                     tokio::time::sleep(RETRY_AFTER).await;
                     error!("Failed to subscribe to Redis: {}", e);
+                    continue;
                 } else {
+                    continue;
                 }
             }
         });
@@ -206,12 +314,8 @@ impl std::fmt::Debug for Redis {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Redis {{ host: {}, port: {}, password: {:?}, db: {}, keys: {:?} }}",
-            self.config.host,
-            self.config.port,
-            self.config.password,
-            self.config.db,
-            self.config.keys
+            "Redis {{ host: {}, password: {:?}, db: {}, keys: {:?} }}",
+            self.config.host, self.config.password, self.config.db, self.config.keys
         )
     }
 }
@@ -223,11 +327,11 @@ mod tests {
     #[test]
     fn test_lookup() {
         let config = RedisConfig {
-            host: "localhost".to_string(),
-            port: 6379,
+            host: "localhost:6379".to_string(),
             password: None,
             db: 9,
             keys: vec!["app_map".to_string()],
+            sentinel_master: None,
         };
         let redis = Redis::new(config).unwrap();
         let result = redis.lookup("test");
